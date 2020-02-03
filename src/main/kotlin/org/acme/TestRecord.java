2708b8b3d1f21b5005f439f589896a06a9742e25
Kevin Wooten <kevin@wooten.com>
2020-02-02 20:21:12 -0700

    initial commit

---
+++ src/main/kotlin/org/acme/TestRecord.java
@@ -0,0 +1,15 @@
+package org.acme;
+
+public class TestRecord {
+
+  private TestType type;
+
+  public TestRecord(TestType type) {
+    this.type = type;
+  }
+
+  public TestType getType() {
+    return type;
+  }
+
+}
---
+++ src/main/kotlin/org/acme/TestType.kt
@@ -0,0 +1,5 @@
+package org.acme
+
+enum class TestType {
+  A, B, C
+}
